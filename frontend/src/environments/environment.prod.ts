46a94bc0f0882d4ff93fc3e2588451eaf1fcca83
Lee Alquisola <ileealquisola@gmail.com>
2025-05-19 21:06:29 +0800

    Update environment.ts and environment.prod.ts in frontend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://usermanagementfinal-production.up.railway.app'
+    apiUrl: 'https://usermanagement-final-el7d.onrender.com'
 }
